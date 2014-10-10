08cf48d21cb1c672d287456cb88aee94949a6d79
Marcus Breese <mbreese@stanford.edu>
2014-10-10 10:01:17 -0700

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='eta',
-      version='0.9.7',
+      version='0.9.8',
       description='ETA Progress bar for command-line utilities',
       author='Marcus Breese',
       author_email='marcus@breese.com',
